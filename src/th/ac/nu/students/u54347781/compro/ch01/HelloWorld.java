3f969d86742869da3124d61d4623f2a6bd1b40ea
Oradee Amrapal <oradee2535@hotmail.com>
2012-07-19 14:20:47 +0700

    HelloWorld.java

---
+++ src/th/ac/nu/students/u54347781/compro/ch01/HelloWorld.java
@@ -0,0 +1,16 @@
+
+package th.ac.nu.students.u54347781.compro.ch01;
+
+/**
+ *
+ * @author User
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hello, World");
+    }
+}
